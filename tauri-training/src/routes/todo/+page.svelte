aabcc23f5d76e3874f3b4fb89effcf6d89743cc6
jeromy-cannon <jeromylewiscannon@gmail.com>
2024-10-06 18:33:57 +0100

    added a basic todo page/route

---
+++ tauri-training/src/routes/todo/+page.svelte
@@ -0,0 +1,26 @@
+<h1>To Do List</h1>
+<table>
+  <tr>
+    <th>Status</th>
+    <th>Item</th>
+  </tr>
+  <tr>
+    <td>[ ]</td> <td>Morning Bathroom Routine</td>
+  </tr>
+  <tr>
+    <td>[ ]</td> <td>IIH Morning Exercises</td>
+  </tr>
+  <tr>
+    <td>[ ]</td> <td>Chiro Stretches</td>
+  </tr>
+  <tr>
+    <td>[ ]</td> <td>Jog/Swim/Yoga</td>
+  </tr>
+  <tr>
+    <td>[ ]</td> <td>IIH Evening Exercises</td>
+  </tr>
+  <tr>
+    <td>[ ]</td> <td>Evening Bathroom Routine</td>
+  </tr>
+</table>
+<a href="/">Home</a>
